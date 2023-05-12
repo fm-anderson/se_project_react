328f14dda5a31c38d3cbe143cb877cccbc17eb5b
AndersonM <andersonmarquesweb@gmail.com>
2023-05-12 13:53:56 -0500

    new component: Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import headerLogo from '../../images/logoHeader.png';
+import avatar from '../../images/avatar.png';
+import './Header.css';
+
+export default function Header() {
+  const currentDate = new Date().toLocaleString('default', {
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <header className="header">
+      <div className="header__info-container">
+        <img className="header__logo" src={headerLogo} alt="logo" />
+        <p className="header__info">{currentDate}, New York</p>
+      </div>
+      <div className="header__user-container">
+        <button
+          className="header__add-button"
+          type="button"
+          //   onClick={onAddButtonClick}
+        >
+          + Add Clothes
+        </button>
+        <div className="header__username">Terrence Tegegne</div>
+        <img className="header__useravatar" src={avatar} alt="avatar" />
+      </div>
+    </header>
+  );
+}
