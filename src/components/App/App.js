44c996c6e073b5e4a51753c06376dcf55300cd75
AndersonM <andersonmarquesweb@gmail.com>
2023-05-12 14:04:32 -0500

    new component: Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>Developed by Anderson M.</div>
+      <div>2023</div>
+    </footer>
+  );
+}
