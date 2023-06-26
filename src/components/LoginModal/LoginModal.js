33afc87d42d2de3ba29e62d98185e4a7f75a3bde
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-06-26 19:53:26 -0400

    new component: LoginModal

---
+++ src/components/LoginModal/LoginModal.js
@@ -0,0 +1,42 @@
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
+import './LoginModal.css';
+
+export default function LoginModal({
+  name,
+  closeModal,
+  handleClickOutsideModal,
+}) {
+  return (
+    <ModalWithForm
+      name={name}
+      title={'Log in'}
+      buttonText={'Log in'}
+      closeModal={closeModal}
+      handleClickOutsideModal={handleClickOutsideModal}
+    >
+      <label className="modal__label">Email</label>
+      <input
+        className="modal__input modal__input_type_text"
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        required
+        minLength="1"
+        maxLength="30"
+      />
+
+      <label className="modal__label">Password</label>
+      <input
+        className="modal__input modal__input_type_text"
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Password"
+        required
+        minLength="8"
+        maxLength="30"
+      />
+    </ModalWithForm>
+  );
+}
